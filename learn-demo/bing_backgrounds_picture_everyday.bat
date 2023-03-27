86bafc03b90eb5fcc2e25bac7904f2b7f84ea2db
th-shenshuxin <shenshuxin@thwb.com.cn>
2023-03-27 13:58:11 +0800

    longtosee-conventions 新增每日获取bing壁纸python脚本并且通过bat脚本启动

---
+++ learn-demo/bing_backgrounds_picture_everyday.bat
@@ -0,0 +1,15 @@
+@echo off
+@REM 把这个文件放到 C:\Users\shenshuxin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup路径下 实现开机自启动
+
+echo '执行每日更换bing壁纸-开始'
+
+
+
+
+timeout 10
+if "%1"=="h" goto begin
+start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
+ 
+python D:\appdata\ssx_private\long_to_see_again\learn-demo\bing_backgrounds_picture_everyday.py
+echo '执行每日更换bing壁纸-完成'
