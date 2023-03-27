1e09a36d4679caebd41918fdce2274624fb7327f
th-shenshuxin <shenshuxin@thwb.com.cn>
2023-03-27 14:00:36 +0800

    设置编码

--- learn-demo/bing_backgrounds_picture_everyday.bat
+++ learn-demo/bing_backgrounds_picture_everyday.bat
@@ -1,5 +1,5 @@
 @echo off
-@REM 把这个文件放到 C:\Users\shenshuxin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup路径下 实现开机自启动
+@REM charset=GB18030 把这个文件放到 C:\Users\shenshuxin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup路径下 实现开机自启动 
 
 echo '执行每日更换bing壁纸-开始'
 
