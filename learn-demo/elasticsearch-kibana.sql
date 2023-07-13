1b1f5d56efdaaf90554c751b557512c75f2536e8
shenshuxin01 <ssx17852015601@163.com>
2023-07-13 08:48:09 +0800

    conventions update July Thursday 0848

--- learn-demo/elasticsearch-kibana.sql
+++ learn-demo/elasticsearch-kibana.sql
@@ -1,11 +1,17 @@
+GET _search
+{
+  "query": {
+    "match_all": {}
+  }
+}
+
 #查看有多少索引库
 GET /_cat/indices?v
 
 #删除索引
-DELETE /logstash-2023.03.1*
+DELETE /logstash-2023.07.0*
 
 
-# 条件查询
 GET logstash*/_search
 {
   "query": {
@@ -14,4 +20,79 @@ GET logstash*/_search
     }
   }
 }
+
+
+GET logstash*/_search
+{
+  "query": {
+    "match_all": {}
+  }
+}
+
+
+# 分组
+GET logstash*/_search
+{
+  "query": {
+    "match": {
+      "traceId": "efd60667e1352684"
+    }
+  },
+  "aggs": {
+    "count": {
+      "terms": {
+        "field": "traceId.keyword"
+      }
+    }
+  }
+}
+
+# 分组
+GET logstash*/_search
+{
+  "query": {
+    "match": {
+      "traceId": "efd60667e1352684"
+    }
+  },
+  "aggs": {
+    "group_by_tags": {
+      "terms": {
+        "field": "traceId.keyword"
+      }
+    }
+  }
+}
+
+# 查询元数据
+GET logstash-*/doc/_mapping
+
+# 查询元数据
+GET logstash-*/_mapping/doc
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
